13db6329c66ee46724410df227ab94a770ea5ee6
Kelvin Kwok <kelvinkwok75@hotmail.com>
2013-07-12 16:11:43 +0000

    modified grader.js to take in URL as input

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,9 +66,14 @@ if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+        .option('-u, --url <url_check>', 'URL') 
+				.parse(process.argv);
+    if(program.checks)
+		  var checkJson = checkHtmlFile(program.file, program.checks);
+    else if(program.url)
+      var checkJson = program.url_check;
     var outJson = JSON.stringify(checkJson, null, 4);
+    //rest.get(apiurl).on('complete', response2console);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
